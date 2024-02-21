eedbe139e6e1f926a7db0a4f2205a3082fbe4611
HectorSandovalLandazuri <88155761+HectorSandovalLandazuri@users.noreply.github.com>
2024-02-21 12:41:43 -0600

    Person and address

---
+++ SimpleWasm/Models/AddressModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SimpleWasm.Models;
+
+public class AddressModel
+{
+    [Required]
+    public string StreetAddress { get; set; }
+	[Required]
+	public string City { get; set; }
+
+	[Required]
+	[StringLength(maximumLength:2,MinimumLength =2)]
+	public string State { get; set; }
+	[Required]
+	[StringLength(maximumLength:10,MinimumLength =5)]
+	public string ZipCode { get; set; }
+
+}
---
+++ SimpleWasm/Models/NameModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SimpleWasm.Models;
+
+public class NameModel
+{
+	[Required]
+	[StringLength(maximumLength: 100, MinimumLength = 1)]
+	public string FirstName { get; set; }
+
+	[Required]
+	[StringLength(maximumLength: 100, MinimumLength = 1)]
+	public string LastName { get; set; }
+
+
+}
